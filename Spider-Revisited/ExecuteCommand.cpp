ab0d80bcc8959025a20ffd5849606ab968a4ff6e
ilan hartskeerl <ilan@hartskeerl.nl>
2021-04-21 13:40:28 +0200

    Dot in comment

--- Spider-Revisited/ExecuteCommand.cpp
+++ Spider-Revisited/ExecuteCommand.cpp
@@ -13,7 +13,7 @@ void ExecuteCommand::exec(const char* cmd)
 {
 	std::array<char, bufferSize> buffer = std::array<char, bufferSize>();
 
-	// Open console to call git commands
+	// Open console to call git commands.
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 	std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd, "r"), _pclose);
 #else
@@ -23,10 +23,10 @@ void ExecuteCommand::exec(const char* cmd)
 	{
 		throw std::runtime_error("popen() failed!");
 	}
-	// Amount of data read, is less then bufferSize if output ends
+	// Amount of data read, is less then bufferSize if output ends.
 	size_t bytesRead;
 
-	// Read until there is nothing more to read, insert chunks into stream
+	// Read until there is nothing more to read, insert chunks into stream.
 
 	while ((bytesRead = fread(buffer.data(), 1, bufferSize, pipe.get())) > 0);
 }
--- Spider-Revisited/Git.cpp
+++ Spider-Revisited/Git.cpp
@@ -13,7 +13,7 @@ int Git::clone(std::string url, std::string filePath)
 	command.append(" && cd " + filePath + " && git sparse-checkout set ");
 	command.append("*.c *.cpp *.h *.cs *.cc *.hpp *.java"); // TODO: read these from file;
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-	// Sadly does not work, although it should
+	// Sadly does not work, although it should.
 	//command.append(" ![Cc][Oo][Nn].* ![Pp][Rr][Nn].* ![Aa][Uu][Xx].* ![Nn][Uu][Ll].* ![Cc][Oo][Mm][123456789].* ![Ll][Pp][Tt][123456789].*");
 #endif
 	command.append(" && git checkout master");
