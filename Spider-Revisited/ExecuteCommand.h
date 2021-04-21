2771ea8b1bf7c64ea50a778198ed1ed5c9dc285b
ilan hartskeerl <ilan@hartskeerl.nl>
2021-04-21 13:30:12 +0200

    copyright

--- Spider-Revisited/ExecuteCommand.h
+++ Spider-Revisited/ExecuteCommand.h
@@ -1,7 +1,15 @@
+/*
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+*/
 #pragma once
 class ExecuteCommand
 {
 public:
+	/// <summary>
+	/// Executes a given command.
+	/// </summary>
 	static void exec(const char* cmd);
 };
 
