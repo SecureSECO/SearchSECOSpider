ad28cf5155d879fdb7c5181f5866db7a42b90196
Floris <f.a.j.dorrestijn@students.uu.nl>
2021-05-14 16:06:17 +0200

    Update Logger.cpp

--- Spider-Revisited/Logger.cpp
+++ Spider-Revisited/Logger.cpp
@@ -1,7 +1,7 @@
 /*This program has been developed by students from the bachelor Computer Science at
 Utrecht University within the Software Project course.
 © Copyright Utrecht University (Department of Information and Computing Sciences)*/
-#include "loguru.hpp"
+#include "../loguru-master/loguru.hpp"
 #include "Logger.h"
 #include <stdlib.h> 
 #define BASE 200
