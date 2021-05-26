5acc56ea42a2b62bf40d651e3ffa7778bb3e392e
Floris <f.a.j.dorrestijn@students.uu.nl>
2021-05-26 11:38:32 +0200

    Changed/added comments and removed unused errortype

--- Spider-Revisited/Error.h
+++ Spider-Revisited/Error.h
@@ -1,9 +1,14 @@
+/*
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+*/
+
 #pragma once
 #include <map>
 #include <string>
 enum class ErrorType
 {
-	GitError,
 	GitCloneError,
 	PipeOpenFailed,
 	BlameIncorrectFormat,
@@ -15,13 +20,17 @@ class Error
 private:
 	inline static std::map<ErrorType, const char*> errorToString =
 	{
-			{ErrorType::GitError, "A git error occured"},
 			{ErrorType::GitCloneError, "Git cloning failed"},
 			{ErrorType::PipeOpenFailed, "popen() failed."},
 			{ErrorType::BlameIncorrectFormat, "Blame data has incorrect format."},
 		{ErrorType::FileExtensionsNotFound, "File extensions file not found. Please re-add this file to your project. No files will be cloned."},
 	};
 public:
+	/// <summary>
+	/// Gets the error message for a given error.
+	/// </summary>
+	/// <param name="e">The error.</param>
+	/// <returns>A specific message for the error.</returns>
 	static const char* getErrorMessage(ErrorType e);
 
 };
