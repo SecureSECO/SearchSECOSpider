b80c4e7ffd12a90a12cf50e56d61aaccc43a7c47
Jochem Bloem <j.bloem1@uu.nl>
2021-06-02 15:56:44 +0200

    Renamed all references to Error.*

--- Spider-Revisited/ErrorSpider.cpp
+++ Spider-Revisited/ErrorSpider.cpp
@@ -3,7 +3,7 @@ This program has been developed by students from the bachelor Computer Science a
 Utrecht University within the Software Project course.
 © Copyright Utrecht University (Department of Information and Computing Sciences)
 */
-#include "Error.h"
+#include "ErrorSpider.h"
 const char* Error::getErrorMessage(ErrorType e)
 {
 	return errorToString[e];
